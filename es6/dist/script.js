04573f3c632f915afe713770f3869e20baf4a971
ypj <406490508@qq.com>
2017-09-30 17:01:48 +0800

    ‘es6-2’

--- es6/dist/script.js
+++ es6/dist/script.js
@@ -1,30 +1,21 @@
 'use strict';
 
-/**
- * 变量的解构赋值。
- * 可以用于 请求返回的json数据。
- */
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-// let a = 0;
-// let b = 1;
-// let c = 2;
+var _ref = [1],
+    a = _ref[0],
+    b = _ref[1],
+    _ref$ = _slicedToArray(_ref[2], 1),
+    z = _ref$[0];
 
-// 和上面是相同的作用,这就相当于解构了一个数组。
-var a = 0,
-    b = 1,
-    c = 2;
+console.log(a);
+console.log(b);
 
-// 但是两边数组的结构必须一样，如果是[d,e,f] = [4,[5,6]] 这样就会报错。
-// 效果等同于 var d = 4,e = 5;f = 6;
+var _j = 'j',
+    _j2 = _slicedToArray(_j, 2),
+    e = _j2[0],
+    _j2$ = _slicedToArray(_j2[1], 1),
+    f = _j2$[0];
 
-var d = 4,
-    e = 5,
-    f = 6;
-
-// 有时解构可以设置一个默认值
-
-var _ref = [],
-    _ref$ = _ref[0],
-    foo = _ref$ === undefined ? 'true' : _ref$;
-
-console.log(foo);
+console.log(e);
+console.log(f);
