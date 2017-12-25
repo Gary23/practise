45cf9c2fe5e3b8cc9275ff1587db7a76fa3c6448
pj.Yu <406490508@qq.com>
2017-12-25 09:11:30 +0800

    ‘20171224’

--- es6/dist/script.js
+++ es6/dist/script.js
@@ -1,75 +1,15 @@
 'use strict';
 
-// 对象扩展运算符
-/**
- *  这是对象扩展运算符的写法，在参数的位置写 ...arg（...后加任意字符），函数内部就可以用arg这个数组来获取参数，主要用于不确定参数长度的情况，实际和arguments的使用很像。 
- */
-function fun() {
-	console.log(arguments.length <= 0 ? undefined : arguments[0]);
-	console.log(arguments.length <= 1 ? undefined : arguments[1]);
-	console.log(arguments.length <= 2 ? undefined : arguments[2]);
-	console.log(arguments.length <= 3 ? undefined : arguments[3]);
-	// 打印 1,2,3,undefined
-}
+// json数组格式
 
-fun(1, 2, 3);
+// es6中，数组的相关操作都放到了Array对象中。jsonp的数组格式key值必须是数字字符串才可以转换为数组。length属性也是必须的。
 
-/**
- * 这里虽然代码上只修改了arr2，但是由于arr1和arr2用一块内存空间，所以实际上也更改了arr1。
- */
-var arr1 = ['a', 'b', 'c'];
-var arr2 = arr1;
-console.log(arr2); // ["a", "b", "c"]
-arr2.push('d');
-console.log(arr1); // ["a", "b", "c", "d"]
+var json = {
+	'0': 'price',
+	'1': 'sale',
+	'2': 'cart',
+	length: 3
+};
 
-
-/**
- * 这样写便解决了上面的问题，...arr3表示将 arr3数组中的元素依次添加到arr4中，也就是说arr4是相当于新建一个数组，然后将arr3中的元素依次添加到新数组中，arr4并没有和arr3指向同一个内存中的空间
- */
-var arr3 = ['a', 'b', 'c'];
-var arr4 = [].concat(arr3);
-console.log(arr4); // ["a", "b", "c"]
-arr2.push('d');
-console.log(arr3); // ["a", "b", "c"]
-
-
-// rest运算符 ...
-/**
- * 从示例可以看出，rest运算符适用于知道前面的一部分参数的数量，而又对于后面剩余的参数数量未知的情况。
- */
-function fun1(first) {
-	console.log(first); // 0
-
-	for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-		arg[_key - 1] = arguments[_key];
-	}
-
-	console.log(arg.length); // 5
-
-	var _iteratorNormalCompletion = true;
-	var _didIteratorError = false;
-	var _iteratorError = undefined;
-
-	try {
-		for (var _iterator = arg[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-			var val = _step.value;
-
-			console.log(val); // 1,2,3,4,5
-		}
-	} catch (err) {
-		_didIteratorError = true;
-		_iteratorError = err;
-	} finally {
-		try {
-			if (!_iteratorNormalCompletion && _iterator.return) {
-				_iterator.return();
-			}
-		} finally {
-			if (_didIteratorError) {
-				throw _iteratorError;
-			}
-		}
-	}
-}
-fun1(0, 1, 2, 3, 4, 5);
+var arr = Array.from(json);
+console.log(arr);
--- es6/js/script.js
+++ es6/js/script.js
@@ -1,48 +1,15 @@
-// 对象扩展运算符
-/**
- *  这是对象扩展运算符的写法，在参数的位置写 ...arg（...后加任意字符），函数内部就可以用arg这个数组来获取参数，主要用于不确定参数长度的情况，实际和arguments的使用很像。 
- */
-function fun(...arg){
-	console.log(arg[0]);
-	console.log(arg[1]);
-	console.log(arg[2]);
-	console.log(arg[3]);
-	// 打印 1,2,3,undefined
+// json数组格式
+
+// es6中，数组的相关操作都放到了Array对象中。jsonp的数组格式key值必须是数字字符串才可以转换为数组。length属性也是必须的。
+
+let json = {
+	'0': 'price',
+	'1': 'sale',
+	'2': 'cart',
+	length: 3
 }
 
-fun(1,2,3);
+let arr = Array.from(json);
+console.log(arr);
 
-
-/**
- * 这里虽然代码上只修改了arr2，但是由于arr1和arr2用一块内存空间，所以实际上也更改了arr1。
- */
-let arr1 = ['a','b','c'];
-let arr2 = arr1;
-console.log(arr2);  // ["a", "b", "c"]
-arr2.push('d');
-console.log(arr1);  // ["a", "b", "c", "d"]
-
-
-/**
- * 这样写便解决了上面的问题，...arr3表示将 arr3数组中的元素依次添加到arr4中，也就是说arr4是相当于新建一个数组，然后将arr3中的元素依次添加到新数组中，arr4并没有和arr3指向同一个内存中的空间
- */
-let arr3 = ['a','b','c'];
-let arr4 = [...arr3];
-console.log(arr4);  // ["a", "b", "c"]
-arr2.push('d');
-console.log(arr3);  // ["a", "b", "c"]
-
-
-// rest运算符 ...
-/**
- * 从示例可以看出，rest运算符适用于知道前面的一部分参数的数量，而又对于后面剩余的参数数量未知的情况。
- */
-function fun1(first,...arg){
-	console.log(first);  // 0
-	console.log(arg.length);  // 5
-
-	for(let val of arg){
-		console.log(val);  // 1,2,3,4,5
-	}
-}
-fun1(0,1,2,3,4,5);
+// 图片url: http://picabstract.preview.ftn.qq.com:8080/ftn_pic_abs_v2/72cfec93fb96c8caf4c2b3b7f48a7345cb2d0845a5290b0d0baf7841a99c1e6f55af7d68fa61cfe2bb27deee992e27b5?pictype=scale&from=30113&version=2.0.0.2&uin=406490508&fname=20171223-4.png&size=1024
