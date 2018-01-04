a6b43f66aa896d4321f44b09c4222fd3eec92d4a
pj.Yu <406490508@qq.com>
2018-01-04 17:53:47 +0800

    '20180104'

--- es6/dist/script.js
+++ es6/dist/script.js
@@ -1,16 +1,40 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+// map的声明和赋值
 
-// Symbol
+// map 比 Set 用的更加广泛。
 
-// Symbol是es6中新增加的数据类型。是原始数据类型。
+// map 的灵活性比 json 更加优秀，像下面这种 json.name 的获取 value 的方式实际上是遍历了整个 json 对象。
 
-// 声明
+var json = {
+	name: 'tim',
+	skill: 'web'
+};
+console.log(json.name); // tim
 
-var s = Symbol();
-console.log(typeof s === 'undefined' ? 'undefined' : _typeof(s)); // Symbol
+// map就是用箭头声明 key 和 value，可以是任何数据类型。
 
-var skill = Symbol('web');
-console.log(skill);
-console.log(skill.toString());
+var map = new Map();
+map.set(json, 'Iam');
+console.log(map); // {{name: "tim", skill: "web"} => "Iam"}
+
+// 上面的例子就体现了 key 可以是任何数据类型，这里 => 前面的就是 key 之后的就是 value。同样 value 也可以是任何数据类型
+
+// map 的长度
+
+// 和数组查看长度一样，通过 size 来查看 map 中有多少条数据。
+
+console.log(map.size); // 1
+
+// map 的增删查
+
+map.set('Iam', json); // {{name: "tim", skill: "web"} => "Iam", "Iam" => {name: "tim", skill: "web"}}
+console.log(map);
+console.log(map.has('Iam')); // true
+console.log(map.has('Iam`')); // false
+console.log(map.get(json)); // Iam
+console.log(map.get('Iam')); // {name: "tim", skill: "web"}
+map.delete(json);
+console.log(map); // {"Iam" => {name: "tim", skill: "web"}}
+map.clear();
+console.log(map); // {}
--- es6/js/script.js
+++ es6/js/script.js
@@ -1,28 +1,31 @@
-// Symbol
+// 用proxy进行预处理
 
-// Symbol 是 es6 中新增加的数据类型。是原始数据类型。
+// Proxy 就是代理的意思。
 
-// 声明
+// Proxy 在 ES6 中作用是增强函数和对象。增强的是声明周期。相当于于一种钩子函数，只不过是在处理任何方法之前进行的，所以叫做预处理。
 
-let s = Symbol();
-console.log(typeof s);   // Symbol
+// es5 中对象的声明和使用
 
-let skill = Symbol('web');
-console.log(skill);   // Symbol(web)   Symbol类型
-console.log(skill.toString());   // Symbol(web)  这里转为了字符串
+let obj = {
+	add: function(val) {
+		return val + 100;
+	},
+	name: 'tim'
+}
+console.log(obj.add(100))   // 200
+console.log(obj.name)   // tim
 
+// es5 这种方式如果要在 obj 声明之前先处理一些东西就要再写一个方法。Proxy 就是为了解决这个问题。
 
-// Symbol 在对象中的应用
+// Proxy 的声明
 
-// 在 node 中的用处比较大，在页面中的用处不是很大
+// Proxy 声明时传递两个参数，第一个就是对象的内容，第二个就是预处理的内容，都是对象的数据结构。
 
+let pro = new Proxy({
+	add: function(val) {
+		return val + 100;
+	},
+	name: 'tim'
+},{
 
-
-
-
-
-
-
-
-
-
+})
