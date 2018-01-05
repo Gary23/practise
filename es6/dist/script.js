cf5a7376308ad8ffe0ccd787b6487acceb2ad50e
pj.Yu <406490508@qq.com>
2018-01-05 17:24:05 +0800

    '20180105'

--- es6/dist/script.js
+++ es6/dist/script.js
@@ -1,46 +1,86 @@
 'use strict';
 
-// Promise
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// 在es5中，有一种回调地狱的比喻，指的就是层级特别深的回调函数，一层函数套着一层函数套了很多层，这样非常不便于后期维护代码。由此便有了es6中的promise。
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-// 要是用 promise 的函数要接收两个参数，resolve（成功时调用） 和 reject（失败时调用），调用时需要传递相应参数，参数可以是字符串、代码、业务逻辑等等。
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var state = 1;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function step1(resolve, reject) {
-	console.log('第一步-注册');
-	if (state == 1) {
-		resolve('第一步-完成');
-	} else {
-		reject('第一步-失败');
+// class 类
+
+// 其实在 es5 中就已经开始模拟 class，而 es6 不需要再模拟了已经实现了。
+
+// 类的名称首字母最好大写，比较规范，类的内部都是方法，如果有属性要写在 constructor 方法里面。最重要的是方法之间不要去加逗号。
+
+// 在类的方法内部，this 指向的是这个类本身，所以可以通过 this 调用类的其他方法。方法之间相互获取值的时候不要忘记 return，负责会是 undefined。
+
+var Coder = function () {
+	function Coder() {
+		_classCallCheck(this, Coder);
 	}
-}
 
-function step2(resolve, reject) {
-	console.log('第二步-登录');
-	if (state == 1) {
-		resolve('第二步-完成');
-	} else {
-		reject('第二步-失败');
+	_createClass(Coder, [{
+		key: 'name',
+		value: function name(val) {
+			console.log(val);
+			return val;
+		}
+	}, {
+		key: 'skill',
+		value: function skill(val) {
+			console.log(this.name('tim') + ':' + 'skill-' + val);
+		}
+	}]);
+
+	return Coder;
+}();
+
+var coder = new Coder();
+
+coder.name('tim'); // tim
+
+coder.skill('web'); // tim:skill-web
+
+// 上面例子中的 val 参数是类函数 name 方法的参数，而不是类的参数。这是有区别的。如果要给类传递参数，需要 constructor 方法，constructor 的参数都是属于类的参数。
+
+var Coder1 = function () {
+	function Coder1(a, b) {
+		_classCallCheck(this, Coder1);
+
+		this.a = a;
+		this.b = b;
 	}
-}
 
-function step3(resolve, reject) {
-	console.log('第三步-购物');
-	if (state == 1) {
-		resolve('第三步-完成');
-	} else {
-		reject('第三步-失败');
+	_createClass(Coder1, [{
+		key: 'add',
+		value: function add() {
+			return this.a + this.b;
+		}
+	}]);
+
+	return Coder1;
+}();
+
+var coder1 = new Coder1(1, 2);
+
+console.log(coder1.add()); // 3
+
+// 类的继承
+
+var htmler = function (_Coder) {
+	_inherits(htmler, _Coder);
+
+	function htmler() {
+		_classCallCheck(this, htmler);
+
+		return _possibleConstructorReturn(this, (htmler.__proto__ || Object.getPrototypeOf(htmler)).apply(this, arguments));
 	}
-}
 
-new Promise(step1).then(function (val) {
-	console.log(val);
-	return new Promise(step2);
-}).then(function (val) {
-	console.log(val);
-	return new Promise(step3);
-}).then(function (val) {
-	console.log(val);
-});
+	return htmler;
+}(Coder);
+
+var coder2 = new htmler();
+
+coder2.name('jack'); //
--- es6/js/script.js
+++ es6/js/script.js
@@ -1,61 +1,53 @@
-// Promise
+// class 类
 
-// 在es5中，有一种回调地狱的比喻，指的就是层级特别深的回调函数，一层函数套着一层函数套了很多层，这样非常不便于后期维护代码。由此便有了es6中的promise。
+// 其实在 es5 中就已经开始模拟 class，而 es6 不需要再模拟了已经实现了。
 
-// 要是用 promise 的函数要接收两个参数，resolve（成功时调用） 和 reject（失败时调用），调用时需要传递相应参数，参数可以是字符串、代码、业务逻辑等等。
+// 类的名称首字母最好大写，比较规范，类的内部都是方法，如果有属性要写在 constructor 方法里面。最重要的是方法之间不要去加逗号。
 
-let state = 1;
+// 在类的方法内部，this 指向的是这个类本身，所以可以通过 this 调用类的其他方法。方法之间相互获取值的时候不要忘记 return，负责会是 undefined。
 
-function step1(resolve, reject) {
-	console.log('第一步-注册');
-	if (state == 1) {
-		resolve('第一步-完成');
-	} else {
-		reject('第一步-失败');
+class Coder {
+	name(val) {
+		console.log(val);
+		return val;
+	}
+	skill(val) {
+		console.log(this.name('tim') + ':' + 'skill-' + val);
 	}
 }
 
-function step2(resolve, reject) {
-	console.log('第二步-登录');
-	if (state == 1) {
-		resolve('第二步-完成');
-	} else {
-		reject('第二步-失败');
+let coder = new Coder;
+
+coder.name('tim');   // tim
+
+coder.skill('web')   // tim:skill-web
+
+// 上面例子中的 val 参数是类函数 name 方法的参数，而不是类的参数。这是有区别的。如果要给类传递参数，需要 constructor 方法，constructor 的参数都是属于类的参数。
+
+class Coder1 {
+	constructor(a, b) {
+		this.a = a;
+		this.b = b;
+	}
+	add() {
+		return this.a + this.b
 	}
 }
 
-function step3(resolve, reject) {
-	console.log('第三步-购物');
-	if (state == 1) {
-		resolve('第三步-完成');
-	} else {
-		reject('第三步-失败');
-	}
+let coder1 = new Coder1(1, 2);
+
+console.log(coder1.add());  // 3
+
+// 类的继承
+
+// 下面的例子是 htmler 类继承自 Coder 类，Coder类是父类。
+
+class htmler extends Coder{
+
 }
 
-new Promise(step1)
-.then(function(val) {
-	console.log(val);
-	return new Promise(step2);
-})
-.then(function(val) {
-	console.log(val);
-	return new Promise(step3);
-})
-.then(function(val) {
-	console.log(val);
-});
+let coder2 = new htmler;
 
-// 最终打印:
-/**
-第一步-注册
-第一步-完成
-第二步-登录
-第二步-完成
-第三步-购物
-第三步-完成
- */
+coder2.name('jack')   // jack
 
-// 上面的例子中 then 方法中的匿名函数的 val 就是 resolve 或者 reject 传递的参数。
-
-// 在 es5 中 step2 肯定是 step1 的回调，step3 肯定是 step2 的回调。如果回调太多，就会很乱难以维护，通过 promise 的 then 方法就解决了这个问题。
+// 这里虽然 htmler 类是空的，但是因为继承自 Coder 类，所以也有 name 方法。
